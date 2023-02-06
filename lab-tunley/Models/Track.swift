1fb68b66c7a2a56393fe42922adcfa77a61335ab
esiguenza98 <esigu001@fiu.edu>
2023-02-05 21:59:58 -0500

    Initial Commit

---
+++ lab-tunley/Models/Track.swift
@@ -0,0 +1,36 @@
+//
+//  Track.swift
+//  lab-tunley
+//
+//  Created by Charlie Hieger on 12/2/22.
+//
+
+import Foundation
+
+// TODO: Pt 1 - Create a Track model struct
+struct Track{
+    
+    let movieName: String
+    let movieOverview: String
+    let voteAverage: Int
+    let voteCount: Int
+    let popularity: Int
+    
+}
+
+// TODO: Pt 1 - Create an extension with a mock tracks static var
+
+
+// MARK: Helper Methods
+/// Converts milliseconds to mm:ss format
+///  ex:  208643 -> "3:28"
+///  ex:
+func formattedTrackDuration(with milliseconds: Int) -> String {
+    let (minutes, seconds) = milliseconds.quotientAndRemainder(dividingBy: 60 * 1000)
+    let truncatedSeconds = seconds / 1000
+    if truncatedSeconds >= 10 {
+        return "\(minutes):\(truncatedSeconds)"
+    } else {
+        return "\(minutes):0\(truncatedSeconds)"
+    }
+}
